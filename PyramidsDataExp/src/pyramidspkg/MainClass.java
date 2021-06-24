e0583050bb0f8f285d78451842dc414caaad311f
AAFat <AAFat@Aboud-Pc>
2021-06-24 20:52:00 +0200

    thread

--- PyramidsDataExp/src/pyramidspkg/MainClass.java
+++ PyramidsDataExp/src/pyramidspkg/MainClass.java
@@ -8,26 +8,36 @@ public class MainClass
 	{
 		String path = "pyramids.csv";
 
-		try
+		new Thread(new Runnable() 
 		{
-			PyramidDao nPyramidsDao = new PyramidDaoImp(); //--- create an instance
-			nPyramidsDao.readAllPyramidsfromCSV(path); //--- Read Data
-			nPyramidsDao.sortPyramids_by_Height(true); //--- Sort by height(descending=true)
-			
-			System.out.println("________ Sorted Pyramids Data by Height ________");
-			int i = 0;
-			for(Pyramid pr:nPyramidsDao.getPyramids())
+			@Override
+			public void run() 
 			{
-				System.out.println("#"+(i++)+pr); 
+				try
+				{
+				PyramidDao nPyramidsDao = new PyramidDaoImp(); //--- create an instance
+				nPyramidsDao.readAllPyramidsfromCSV(path); //--- Read Data
+				nPyramidsDao.sortPyramids_by_Height(true); //--- Sort by height(descending=true)
+				
+				System.out.println("________ Sorted Pyramids Data by Height ________");
+				int i = 0;
+				for(Pyramid pr:nPyramidsDao.getPyramids())
+				{
+					System.out.println("#"+(i++)+pr); 
+				}
+				
+				System.out.println("________ Number of Pyramids in each Site ________");
+				System.out.println(nPyramidsDao.getSites_Dict());
+				}
+				catch(IOException e)
+				{
+					e.printStackTrace();
+				}
 			}
+		}).start();
 			
-			System.out.println("________ Number of Pyramids in each Site ________");
-			System.out.println(nPyramidsDao.getSites_Dict());
 
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
+
+ 
 	}
 }
